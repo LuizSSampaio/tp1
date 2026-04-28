1d4678d7407737a72f10ccb9c39939797105de9b
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-04-28 18:25:28 -0300

    feat: get input

--- tp.c
+++ tp.c
@@ -6,9 +6,25 @@ int main() {
     int terms;
     if (scanf("%d", &terms) == -1) {
         printf("Falha ao ler a quantidade de termos\n");
+        return -1;
     }
 
     Polinomio pol = criarPolinomio(terms);
+    for (int i = 0; i < terms; i++) {
+        int coe, exp;
+        if (scanf("%d%d", &coe, &exp) == -1) {
+            printf("Falha ao ler termos\n");
+            return -1;
+        }
+
+        if (exp < 0) {
+            printf("Erro: expoente negativo.");
+            return 0;
+        }
+
+        pol.termos[i].coeficiente = coe;
+        pol.termos[i].expoente = exp;
+    }
 
     destroiPolinomio(&pol);
     return 0;
