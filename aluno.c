00b7090dc6f12ab8f1878e7222a008ae877b97b4
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-04-28 17:48:28 -0300

    feat: handle creation and deletion of Polinomio

--- aluno.c
+++ aluno.c
@@ -1,1 +1,13 @@
+#include <stdlib.h>
+
 #include "aluno.h"
+
+Polinomio criarPolinomio(int size) {
+    Polinomio pol;
+    pol.termos = malloc(sizeof(Termo) * size);
+    pol.size = size;
+
+    return pol;
+}
+
+void destroiPolinomio(Polinomio self) { free(self.termos); }
